13457cff944a1a4c81b9bda4a3f47aa3c1e2e8c0
Ugesh Kumar KS <ugesh262000@gmail.com>
2023-10-20 19:33:18 +0530

    [ugesh] First Commit

---
+++ Day21_PracticeProblem/Delete_a_Customer.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day21_PracticeProblem
+{
+    internal class Delete_a_Customer
+    {
+    }
+}
---
+++ Day21_PracticeProblem/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Day21_PracticeProblem
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
